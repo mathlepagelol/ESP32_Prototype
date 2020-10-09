d896d97bb3f62825be9deb7f549907a059f698ab
Mathieu Lepage <mat_182_42@hotmail.com>
2020-10-09 17:22:46 -0400

    v2.0

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 
+//La LED est set à la PIN2
 const int led = 2;
 unsigned int compteur = 0;
 
@@ -8,7 +9,7 @@ void setup() {
   Serial.println("Test");
   pinMode(led, OUTPUT);
 }
-
+//Fait flash la lED à chaque seconde
 void loop() {
   Serial.println("Boucle : " + String(compteur));
   digitalWrite(led, HIGH);
