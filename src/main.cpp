d72fbc77f9f66ef1b9af115495046a3e07ea3dab
Mathieu Lepage <mat_182_42@hotmail.com>
2020-10-09 17:14:30 -0400

    initialisation

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+#include <Arduino.h>
+
+const int led = 2;
+unsigned int compteur = 0;
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Test");
+  pinMode(led, OUTPUT);
+}
+
+void loop() {
+  Serial.println("Boucle : " + String(compteur));
+  digitalWrite(led, HIGH);
+  delay(1000);
+  digitalWrite(led, LOW);
+  delay(1000);
+  compteur++;
+}
