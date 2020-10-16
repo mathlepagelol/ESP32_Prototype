f0f535b03c44a355f3b355c0152095bae03566cb
Mathieu Lepage <mat_182_42@hotmail.com>
2020-10-16 19:59:35 -0400

    ajout de commentaires

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,8 @@
+// NOM DU PROGRAMME : ESP32_PROTOTYPE
+// REALISATION INFORMATIQUE : Mathieu Lepage
+// DATE DE CREATION : 2020/10/16
+// OBJET : Programme test permettant d'allumer/éteindre la LED du ESP32 en continue
+
 #include <Arduino.h>
 
 //La LED est set à la PIN2
@@ -5,11 +10,13 @@ const int led = 2;
 unsigned int compteur = 0;
 
 void setup() {
-  Serial.begin(115200);
+  //Permet d'afficher "Test" dans le serial pour vérifier que le moniteur série est fonctionnel
+  Serial.begin(9600);
   Serial.println("Test");
+  //Configure la PIN spécifique pour qu'elle se comporte comme une sortie
   pinMode(led, OUTPUT);
 }
-//Fait flash la lED à chaque seconde
+//Fait flash la LED à chaque seconde
 void loop() {
   Serial.println("Boucle : " + String(compteur));
   digitalWrite(led, HIGH);
